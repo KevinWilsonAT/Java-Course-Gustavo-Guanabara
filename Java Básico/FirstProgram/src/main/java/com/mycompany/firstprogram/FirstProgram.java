42bf7b34561e99ab6bb30b710102779e5aae469d
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-02-14 23:46:10 -0300

    upload classes 14-02-24

---
+++ Java Básico/FirstProgram/src/main/java/com/mycompany/firstprogram/FirstProgram.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.firstprogram;
+
+/**
+ * @author kevin
+ */
+
+/**
+* Camel Case General Rules:
+* 
+* Classes / Interfaces = always words 1st characters uppercase. Ex.: MyClass
+* Attributes / Variables / Methods = 1st word character lowercase and others uppercase.
+*                                    Ex.: myAttribute, myVariable, myMethod
+* Packages = all lowercase. Ex.: mypackage
+* Constant = all uppercase. Ex.: MYCONSTANT
+*/
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
