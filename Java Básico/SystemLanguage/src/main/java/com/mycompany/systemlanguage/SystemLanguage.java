2793340409b15a445b5d268ceb3ffd3751b2518d
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-02-16 10:58:14 -0300

    update 16-02-24

---
+++ Java Básico/SystemLanguage/src/main/java/com/mycompany/systemlanguage/SystemLanguage.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.systemlanguage;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author kevin
+ */
+
+public class SystemLanguage {
+
+    public static void main(String[] args) {
+        Locale lang = Locale.getDefault();
+        
+        System.out.println("The system language is:");
+        System.out.println(lang);
+    }
+}
