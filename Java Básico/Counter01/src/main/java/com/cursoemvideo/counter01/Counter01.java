31b3ae519b361f52bfd71a88c936e0d8f7454f30
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2024-03-04 17:32:58 -0300

    upload classes 02-03-24

---
+++ Java Básico/Counter01/src/main/java/com/cursoemvideo/counter01/Counter01.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.cursoemvideo.counter01;
+
+/**
+ *
+ * @author kevin
+ */
+public class Counter01 {
+
+    public static void main(String[] args) {
+        int sc = 0;
+        while (sc < 4){
+            System.out.println("Preparing Somersault #" + sc);
+            System.out.println("Doing Somersault #" + sc);
+            System.out.println("Somersault #" + sc + " done");
+            System.out.println("=============================");
+            sc++;
+        }
+    }
+}
